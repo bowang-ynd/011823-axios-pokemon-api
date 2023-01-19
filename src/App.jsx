74a3753b417fbeeaf7e12da04e7d969d72ad32fe
YND <yamatonodo@gmail.com>
2023-01-18 20:49:00 -0800

    fitst commit for javascript practice: axios fetch pokemon api

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FetchPokemon from './components/FetchPokemon'
+
+const App = () => {
+    return (
+        <div className='container'>
+            <FetchPokemon />
+        </div>
+    )
+}
+
+export default App
